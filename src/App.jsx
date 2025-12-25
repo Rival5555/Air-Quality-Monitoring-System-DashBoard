5723b6fbb7d0b46ee6fd29a3bc7e58dd95ceee0b
Rival5555 <hassanali93r@gmail.com>
2025-12-26 01:33:35 +0500

    adding some additional functions in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { Toaster } from 'react-hot-toast'; // Import Toaster
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
 
 function App() {
   const [darkMode, setDarkMode] = useState(() => {
@@ -27,9 +29,13 @@ function App() {
   const toggleTheme = () => setDarkMode(!darkMode);
 
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-200 font-sans">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-200 font-sans flex flex-col">
+      <Toaster position="top-right" reverseOrder={false} /> {/* Add Toaster here */}
       <Header darkMode={darkMode} toggleTheme={toggleTheme} />
-      <Dashboard darkMode={darkMode} />
+      <main className="flex-grow">
+        <Dashboard darkMode={darkMode} />
+      </main>
+      <Footer />
     </div>
   );
 }
